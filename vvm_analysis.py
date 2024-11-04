c46119ebbc1e7a92388152589d2214e4b5f24f04
yuchung0823 <yuzhongzhng@gmail.com>
2024-11-04 22:30:13 +0800

    find boundary +0.5K

--- vvm_analysis.py
+++ vvm_analysis.py
@@ -22,7 +22,7 @@ class newVVMTools(vvmtools.analyze.DataRetriever):
         TKE = np.mean(u_inter ** 2 + v_inter ** 2 + w_inter ** 2, axis=(1, 2))
         return TKE
     
-    def Enstrophy(self, t, func_config):
+    def cal_Enstrophy(self, t, func_config):
         zeta = np.squeeze(self.get_var("zeta", t, numpy=True,domain_range=func_config["domain_range"]))
         eta = np.squeeze(self.get_var("eta", t, numpy=True,domain_range=func_config["domain_range"]))
         xi = np.squeeze(self.get_var("xi", t, numpy=True,domain_range=func_config["domain_range"]))        
@@ -33,4 +33,36 @@ class newVVMTools(vvmtools.analyze.DataRetriever):
         xi_inter = (xi[1:, :, :] + xi[:-1, :, :] + xi[:, 1:, :] + xi[:, :-1, :])[:, :, 1:] / 4
         return np.nanmean((eta_inter**2 + zeta_inter**2 + xi_inter**2),axis = (1,2))
 
-testtools=newVVMTools('/data/mlcloud/r13229010/VVM/DATA/case_HW5a')
+    def cal_wth(self, t, func_config):
+        w = np.squeeze(self.get_var("w", t, numpy=True,domain_range=func_config["domain_range"]))
+        th = np.squeeze(self.get_var("th", t, numpy=True,domain_range=func_config["domain_range"]))[1:]
+        w_inter = (w[1:] + w[:-1]) / 2
+        wbar = np.squeeze(self.get_var("w", t, numpy=True,compute_mean=True,axis=(1,2)))
+        wbar_inter = (wbar[1:] + wbar[:-1]) / 2
+        thbar = np.squeeze(self.get_var("th", t, numpy=True,compute_mean=True,axis=(1,2)))[1:]
+        wprime = w_inter - wbar_inter.reshape(len(wbar_inter),1,1)
+        thprime = th - thbar.reshape(len(thbar),1,1)
+        wthbar = np.mean(wprime * thprime,axis=(1,2))
+        return wthbar
+
+    def find_BL_boundary(self, var, howToSearch, threshold=0.01):
+        zc = self.DIM["zc"]
+        zc_expand = np.meshgrid(zc,var[:,0],indexing='ij')[0]
+        print(np.shape(zc),np.shape(var[:,0]),np.shape(zc_expand))
+        if howToSearch == "th_plus05K":
+            index = np.argmin(np.abs(var - (var[:,0].reshape(len(var[:,0]),1) + 0.5)),axis=1)
+            height = zc_expand[index]
+            return height
+
+
+        elif howToSearch == "dthdz":
+            pass
+
+        elif howToSearch == "threshold":
+            pass
+
+        elif howToSearch == "wth":
+            pass
+
+        else:
+            print("Without this searching approach")
