86f6b77110e2e164b3039805333e58744f6bcf0c
r13229010 <r13229010@ntu.edu.tw>
2024-11-08 11:21:25 +0800

    fix the dimention error

--- main.py
+++ main.py
@@ -1,12 +1,58 @@
 from vvm_analysis import newVVMTools
+from vvmtools.plot import DataPlotter
 import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
+
 testtools=newVVMTools('/data/mlcloud/r13229010/VVM/DATA/case_HW5a')
-func_config = {"domain_range": (None, None, None, None, 64, 128)}
+func_config = {"domain_range": (None, None, None, None, None, None)}
 TKE_land = testtools.func_time_parallel(func=testtools.cal_wth,time_steps=list(range(0, 721, 1)), func_config=func_config, cores=10)
 th = testtools.get_var_parallel('th',time_steps=list(range(0, 721, 1)),domain_range=func_config['domain_range'],compute_mean=True, axis=(1,2))
+TKE_real = testtools.func_time_parallel(func=testtools.cal_TKE,time_steps=list(range(0, 721, 1)), func_config=func_config, cores=10)
+Enstrophy_real = testtools.func_time_parallel(func=testtools.cal_Enstrophy,time_steps=list(range(0, 721, 1)), func_config=func_config, cores=10)
+wth_bar = testtools.func_time_parallel(func=testtools.cal_wth,time_steps=list(range(0, 721, 1)), func_config=func_config, cores=10)
 t05 = testtools.find_BL_boundary(th, "th_plus05K")
-plt.pcolormesh(np.arange(721), testtools.DIM["zc"][1:], TKE_land.T)
-plt.plot(t05)
-plt.show()
+dthdz = testtools.find_BL_boundary(th, "dthdz")
+tke= testtools.find_BL_boundary(TKE_real, "threshold",threshold=0.1)
+Enstrophy = testtools.find_BL_boundary(Enstrophy_real, "threshold",threshold=0.1)
+wth_pn, wth_min, wth_np, indice_02 = testtools.find_BL_boundary(wth_bar, "wth",threshold=1e-2)
+print("t05.shape: " + str(t05.shape))
+boundary_dict = {                   
+    "θ + 0.5K": t05,                
+    "max dθ/dz": dthdz,            
+    "TKE": tke,
+    "Enstrophy": Enstrophy,      
+    "top(w′θ′ +)": wth_pn,         
+    "min(w′θ′ )": wth_min,         
+    "top(w′θ′ -)": wth_np         
+}
+
+# prepare expname and data coordinate
+nx = 128; x = np.arange(nx)*0.2
+ny = 128; y = np.arange(ny)*0.2
+nz = 50;  z = np.arange(nz)*0.04
+nt = 721; t = np.arange(nt)*np.timedelta64(2,'m')+np.datetime64('2024-01-01 05:00:00')
+time = np.arange(nt)
+
+
+expname  = 'HW7a'
+figpath           = './fig/'
+data_domain       = {'x':x, 'y':y, 'z':z, 't':t}
+data_domain_units = {'x':'km', 'y':'km', 'z':'m', 't':'LocalTime'}
+plotter = DataPlotter(exp=expname, figpath=figpath, domain=data_domain ,
+         units=data_domain_units )
+#for key, value in boundary_dict.items():
+#    print(f"{key} size:", len(value))
+wth_bar = np.hstack((np.full((nt,1),np.nan), wth_bar))
+plotter.draw_zt(data = wth_bar.T, \
+                      levels=np.arange(-0.04,0.041,0.005), \
+                      extend='both', \
+                      pblh_dicts=boundary_dict,\
+                      cmap_name='bwr',\
+                      title_left = 'vertical θ transport', \
+                      title_right = 'PE whole domain', \
+                      xlim = None, \
+                      ylim = None,\
+                      figname='HW7.1.png',\
+               )
+
--- vvm_analysis.py
+++ vvm_analysis.py
@@ -25,13 +25,19 @@ class newVVMTools(vvmtools.analyze.DataRetriever):
     def cal_Enstrophy(self, t, func_config):
         zeta = np.squeeze(self.get_var("zeta", t, numpy=True,domain_range=func_config["domain_range"]))
         eta = np.squeeze(self.get_var("eta", t, numpy=True,domain_range=func_config["domain_range"]))
-        xi = np.squeeze(self.get_var("xi", t, numpy=True,domain_range=func_config["domain_range"]))        
-        if zeta.shape != eta:
+        xi = np.squeeze(self.get_var("xi", t, numpy=True,domain_range=func_config["domain_range"]))   
+        if zeta.shape != eta.shape:
             eta = np.squeeze(self.get_var("eta_2", t, numpy=True,domain_range=func_config["domain_range"]))
-        zeta_inter = (zeta[:, :, 1:] + zeta[:, :, :-1] + zeta[:, 1:, :] + zeta[:, :-1, :])[1:, :, :] / 4
-        eta_inter = (eta[:, :, 1:] + eta[:, :, :-1] + eta[1:, :, :] + eta[:-1, :, :])[:, 1:, :] / 4
-        xi_inter = (xi[1:, :, :] + xi[:-1, :, :] + xi[:, 1:, :] + xi[:, :-1, :])[:, :, 1:] / 4
-        return np.nanmean((eta_inter**2 + zeta_inter**2 + xi_inter**2),axis = (1,2))
+        # zeta_inter = (zeta[:, :, 1:] + zeta[:, :, :-1] + zeta[:, 1:, :] + zeta[:, :-1, :])[1:, :, :] / 4
+        # eta_inter = (eta[:, :, 1:] + eta[:, :, :-1] + eta[1:, :, :] + eta[:-1, :, :])[:, 1:, :] / 4
+        # xi_inter = (xi[1:, :, :] + xi[:-1, :, :] + xi[:, 1:, :] + xi[:, :-1, :])[:, :, 1:] / 4
+        
+        xi_inter = (xi[:, 1:, 1:] + xi[:, :-1, 1:] + xi[:, 1:, :-1] + xi[:, :-1, :-1])[1:] / 4
+        eta_inter = (eta[1:, :, 1:] + eta[:-1, :, 1:] + eta[1:, :, :-1] + eta[:-1, :, :-1])[:, 1:] / 4
+        zeta_inter = (zeta[1:, 1:] + zeta[:-1, 1:] + zeta[1:, :-1] + zeta[:-1, :-1])[:, :, 1:] / 4
+ 
+        E =  np.mean(eta_inter ** 2 + zeta_inter ** 2 + xi_inter ** 2, axis=(1, 2))
+        return E
 
     def cal_wth(self, t, func_config):
         w = np.squeeze(self.get_var("w", t, numpy=True,domain_range=func_config["domain_range"]))
@@ -46,23 +52,55 @@ class newVVMTools(vvmtools.analyze.DataRetriever):
         return wthbar
 
     def find_BL_boundary(self, var, howToSearch, threshold=0.01):
-        zc = self.DIM["zc"]
+        zc = self.DIM["zc"]/1000
         zc_expand = np.meshgrid(zc,var[:,0],indexing='ij')[0]
-        print(np.shape(zc),np.shape(var[:,0]),np.shape(zc_expand))
         if howToSearch == "th_plus05K":
             index = np.argmin(np.abs(var - (var[:,0].reshape(len(var[:,0]),1) + 0.5)),axis=1)
-            height = zc_expand[index]
+            print(index)
+            height = zc[index]
+            
             return height
 
 
         elif howToSearch == "dthdz":
-            pass
+            var_gradient = np.gradient(var, zc, axis=1)
+            max_gradient_indices = np.argmax(var_gradient, axis=1)
+            max_gradient_heights = zc[max_gradient_indices]
+            print('shape of max gradient heights: ',np.shape(max_gradient_heights) )
+            return max_gradient_heights
 
         elif howToSearch == "threshold":
-            pass
+            boundary_heights = np.zeros(len(var[:,0]))
+            for timestep in range(len(var[:,0])):
+                if np.max(np.abs(var[timestep])) >= threshold:
+                    boundary_heights[timestep] = zc[np.argmin(np.abs(var[timestep]-threshold))]
+                else:
+                    boundary_heights[timestep] = np.nan
+            return boundary_heights
 
         elif howToSearch == "wth":
-            pass
+            boundary_pn = np.zeros(len(var[:,0]))
+            boundary_min = np.zeros(len(var[:,0]))
+            boundary_np = np.zeros(len(var[:,0]))
+            indice_03 = np.zeros(len(var[:,0]))
+            for timestep in range(len(var[:,0])):
+                if np.max(np.abs(var[timestep])) <= threshold:
+                    boundary_pn[timestep] = 0
+                    boundary_min[timestep] = 0
+                    boundary_np[timestep] = 0
+                elif np.max(var[timestep, np.argmax((var[timestep])):]) < threshold:
+                    print(np.max(var[timestep, np.argmax((var[timestep])):]))
+                    boundary_min[timestep] = zc[np.argmin(var[timestep])]
+                    boundary_pn[timestep] = zc[np.argmin(np.abs(var[timestep, 1:np.argmin(var[timestep])]))]
+                    boundary_np[timestep] = 0#np.nan
+                else:
+                    boundary_min[timestep] = zc[np.argmin(var[timestep])]
+                    boundary_pn[timestep] = zc[np.argmin(np.abs(var[timestep, 1:np.argmin(var[timestep])]))]
+                    indice_01 = np.argmin(var[timestep]) # minimum
+                    indice_02 = indice_01 + np.argmax((var[timestep, indice_01:])) # second large
+                    boundary_np[timestep] = zc[indice_01 + np.argmin(np.abs(var[timestep, indice_01:indice_02] + threshold))]
+                    indice_03[timestep] = (zc[indice_02])
+            return boundary_pn, boundary_min, boundary_np, indice_03
 
         else:
             print("Without this searching approach")
